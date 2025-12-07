4024d9e16db78f1c44c10294520bd22794a90599
MORA DIGITAL AUTOMATIONS <moradigitalautomations@gmail.com>
2025-12-07 14:11:30 +0530

    Create app.tsx

---
+++ app.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { HashRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { Layout } from './components/Layout';
+import Home from './pages/Home';
+import Services from './pages/Services';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import ThankYou from './pages/ThankYou';
+
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+  React.useEffect(() => { window.scrollTo(0, 0); }, [pathname]);
+  return null;
+};
+
+const App: React.FC = () => (
+  <Router>
+    <ScrollToTop />
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/thank-you" element={<ThankYou />} />
+      </Routes>
+    </Layout>
+  </Router>
+);
+
+export default App;
